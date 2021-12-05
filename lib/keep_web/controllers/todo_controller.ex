ff563d1d594b8ad2879feafda0abbd7426f91a2f
mudssrali <mudassaralics007@gmail.com>
2021-12-05 17:13:22 +0500

    get todos in descending order and pass down to template

--- lib/keep_web/controllers/todo_controller.ex
+++ lib/keep_web/controllers/todo_controller.ex
@@ -1,13 +1,16 @@
 defmodule KeepWeb.TodoController do
-    use KeepWeb, :controller
-  
-    def index(conn, _params) do
-      render(conn, "index.html")
-    end
+  use KeepWeb, :controller
 
-    def new(conn, _params) do
-        render(conn, "new.html")
-    end
+  alias Keep.Todo
 
+  @doc false
+  def index(conn, _params) do
+    todos = Todo.all()
+    render(conn, "index.html", todos: todos)
   end
-  
+
+  @doc false
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+end
