4610c38a2c4e5c43218333a664b1e1250e97d1d8
mudssrali <mudassaralics007@gmail.com>
2021-12-05 20:43:22 +0500

    add todo resolvers and types empty module

---
+++ server/lib/keep_web/graphql/todo/todo_resolvers.ex
@@ -0,0 +1,8 @@
+defmodule KeepWeb.GraphQL.TodoResolvers do
+    use KeepWeb.GraphQL.Errors
+    
+    require Logger
+
+    alias KeepWeb.GraphQL.EctoHelpers
+    alias KeepWeb.Todo
+end
---
+++ server/lib/keep_web/graphql/todo/todo_types.ex
@@ -0,0 +1,13 @@
+defmodule KeepWeb.GraphQL.TodoTypes do
+    use Absinthe.Schema.Notation
+
+    alias KeepWeb.GraphQL.TodoResolvers
+
+    object :todo_queries do
+        
+    end
+
+    object :todo_mutations do
+        
+    end
+end
