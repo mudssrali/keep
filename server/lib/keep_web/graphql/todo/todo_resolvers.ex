309003d908b804cbefeb98ebad29e564cd7030d6
mudssrali <mudassaralics007@gmail.com>
2021-12-05 23:55:29 +0500

    add todo list and item absinthe types and resolvers

--- server/lib/keep_web/graphql/todo/todo_resolvers.ex
+++ server/lib/keep_web/graphql/todo/todo_resolvers.ex
@@ -1,8 +1,83 @@
 defmodule KeepWeb.GraphQL.TodoResolvers do
-    use KeepWeb.GraphQL.Errors
-    
-    require Logger
+  use KeepWeb.GraphQL.Errors
 
-    alias KeepWeb.GraphQL.EctoHelpers
-    alias KeepWeb.Todo
+  alias KeepWeb.GraphQL.EctoHelpers
+  alias Keep.Todo
+
+  def get_todos(_parent, _args, _context) do
+    todos = Todo.all()
+    {:ok, %{todos: todos}}
+  end
+
+  def get_todo_list(_parent, %{list_id: id} = args, _context) do
+    {:ok, Todo.get_list!(id)}
+  end
+
+  def create_todo_list(_parent, %{todo_list: list} = args, _context) do
+    Todo.create_list(list)
+  end
+
+  def update_todo_list(_parent, args, _context) do
+    %{
+      todo_list: %{
+        id: id,
+        title: title
+      }
+    } = args
+
+    Todo.update_list(id, %{title: title})
+  end
+
+  def update_todo_list_status(_parent, args, _context) do
+    %{
+      todo_list: %{
+        id: id,
+        archived: archived
+      }
+    } = args
+
+    list = Todo.update_list_status(id, archived)
+    {:ok, list}
+  end
+
+  def create_todo_item(_parent, %{todo_item: item} = args, _context) do
+    case Todo.create_item(item) do
+      {:ok, list} ->
+        {:ok, list}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  def update_todo_item(_parent, args, _context) do
+   %{
+     todo_item:  %{
+      id: id,
+      content: content
+    }
+   } = args
+
+    case Todo.update_item(id, %{content: content}) do
+      {:ok, item} ->
+        {:ok, item}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  def update_todo_item_status(_parent, args, _context) do
+    %{
+      todo_item: %{
+        id: id,
+        completed: completed
+      }
+    } = args
+
+    case Todo.update_item_status(id, completed) do
+      {:ok, item} ->
+        {:ok, item}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
 end
--- server/lib/keep_web/graphql/todo/todo_types.ex
+++ server/lib/keep_web/graphql/todo/todo_types.ex
@@ -1,13 +1,110 @@
 defmodule KeepWeb.GraphQL.TodoTypes do
-    use Absinthe.Schema.Notation
+  @moduledoc """
+  Provides graphql types for Todo
+  """
 
-    alias KeepWeb.GraphQL.TodoResolvers
+  use Absinthe.Schema.Notation
 
-    object :todo_queries do
-        
+  alias KeepWeb.GraphQL.TodoResolvers
+
+  object :todo_queries do
+    @desc "get all todos"
+    field :get_todos, :todos_data do
+      resolve(&TodoResolvers.get_todos/3)
     end
 
-    object :todo_mutations do
-        
+    @desc "get todo list with items"
+    field :get_todo, :todo_list do
+      arg(:list_id, non_null(:string))
+      resolve(&TodoResolvers.get_todo_list/3)
     end
+  end
+
+  object :todo_mutations do
+    @desc "create a new todo list"
+    field :create_todo_list, :todo_list do
+      arg(:todo_list, non_null(:create_todo_list))
+      resolve(&TodoResolvers.create_todo_list/3)
+    end
+
+    @desc "update an existing todo list"
+    field :update_todo_list, :todo_list do
+      arg(:todo_list, non_null(:update_todo_list))
+      resolve(&TodoResolvers.update_todo_list/3)
+    end
+
+    @desc "update status of a todo list"
+    field :update_todo_list_status, :todo_list do
+      arg(:todo_list, non_null(:update_todo_list_status))
+      resolve(&TodoResolvers.update_todo_list_status/3)
+    end
+
+    @desc "create a new todo item"
+    field :create_todo_item, :todo_item do
+      arg(:todo_item, non_null(:create_todo_item))
+      resolve(&TodoResolvers.create_todo_item/3)
+    end
+
+    @desc "update an existing todo item"
+    field :update_todo_item, :todo_item do
+      arg(:todo_item, non_null(:update_todo_item))
+      resolve(&TodoResolvers.update_todo_item/3)
+    end
+
+    @desc "update status of a todo item"
+    field :update_todo_item_status, :todo_item do
+      arg(:todo_item, non_null(:update_todo_item_status))
+      resolve(&TodoResolvers.update_todo_item_status/3)
+    end
+  end
+
+ input_object :create_todo_list do
+    field(:title, :string)
+  end
+
+ input_object :update_todo_list do
+    field(:id, :string)
+    field(:title, :string)
+  end
+
+ input_object :update_todo_list_status do
+    field(:id, :string)
+    field(:archived, :string)
+  end
+
+ input_object :create_todo_item do
+    field(:list_id, :string)
+    field(:content, :string)
+  end
+
+ input_object :update_todo_item do
+    field(:id, :string)
+    field(:content, :string)
+  end
+
+ input_object :update_todo_item_status do
+    field(:id, :string)
+    field(:completed, :string)
+  end
+
+  object :todo_item do
+    field(:id, :string)
+    field(:content, :string)
+    field(:completed, :boolean)
+    field(:inserted_at, :date)
+    field(:updated_at, :date)
+  end
+
+  object :todo_list do
+    field(:id, :string)
+    field(:title, :string)
+    field(:archived, :boolean)
+    field(:inserted_at, :date)
+    field(:updated_at, :date)
+    field(:items, list_of(:todo_item))
+  end
+
+  object :todos_data do
+    field(:todos, list_of(:todo_list))
+  end
 end
