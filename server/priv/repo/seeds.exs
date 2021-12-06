bf6b1e721ec5ff5712f24d555e521dcfa150f503
mudssrali <mudassaralics007@gmail.com>
2021-12-06 22:50:22 +0500

    add todo seeds

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -1,11 +1,28 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Keep.Repo.insert!(%Keep.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Keep.Todo
+
+# Some fake todos
+
+todos = %{
+  "Programming Books" => [
+    "Elixir in Action",
+    "Started with Elixir",
+    "Elixir Phoenix Web Programming",
+    "Distributed Systems"
+  ],
+  "Crypto Currencies" => ["Bitcoin", "Dogecoin", "Shibcoin", "Ethereum"],
+  "Countries" => ["Pakistan", "Turkey", "America", "Iran", "Saudi Arabia"],
+  "Wild Animals" => ["Lion", "Tiger", "Giraffe", "Zebra", "Hippo", "Lynx", "Fox"],
+  "Random Facts" => [
+    "Coding has over 700 languages.",
+    "Coding Bugs were NOT named after an actual bug.",
+    "Coding will soon be as important as reading.",
+    "The first computer virus was a Creeper."
+  ]
+}
+
+# Create todos
+
+todos
+|> Enum.each(fn {k, v} ->
+  Todo.create_list_with_items(%{title: k}, v)
+end)
