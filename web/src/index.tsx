0e28e4da8dbd1c1aa58065d73049e2073e5a7d14
mudssrali <mudassaralics007@gmail.com>
2021-12-07 03:47:51 +0500

    configure react-host-toast

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Toaster } from 'react-hot-toast'
 import Routes from './routing'
 import './styles/app.css'
 import './styles/helper.css'
 
-ReactDOM.render(<Routes />, document.getElementById('keep'))
+ReactDOM.render(
+	<>
+		<Toaster
+			position={'top-right'}
+			toastOptions={{
+				style: {
+					margin: '15px',
+					background: '#363636',
+					color: '#fff',
+					width: '340px'
+				},
+				className: 'text-base',
+				duration: 3000
+			}}
+		/>
+		<Routes />
+	</>,
+	document.getElementById('keep')
+)
