916f88b69d25d7d5a4c63cda24253d2b1adce251
mudssrali <mudassaralics007@gmail.com>
2021-12-07 16:17:31 +0500

    add more tests,  update description and a bit refactor

--- server/test/keep_web/controllers/todo_controller_test.exs
+++ server/test/keep_web/controllers/todo_controller_test.exs
@@ -4,7 +4,7 @@ defmodule KeepWeb.TodoControllerTest do
   alias Keep.Repo
   alias Keep.Todo
 
-  describe "GET api/lists/ api/list?id=" do
+  describe "Public API - GET" do
     @valid_attrs %{title: "Coding Styles"}
 
     def list_fixture(attrs \\ %{}) do
@@ -15,7 +15,7 @@ defmodule KeepWeb.TodoControllerTest do
       end
     end
 
-    test "get_lists renders a todo lists" do
+    test "GET /api/lists" do
       conn = build_conn()
       conn = get(conn, Routes.todo_path(conn, :get_lists))
       assert json_response(conn, 200)
@@ -28,16 +28,14 @@ defmodule KeepWeb.TodoControllerTest do
              }
     end
 
-    test "get_list renders a single todo list" do
+    test "GET /api/list?=id" do
       conn = build_conn()
       todo = list_fixture()
 
       conn = get(conn, Routes.todo_path(conn, :get_list, %{id: todo.id}))
 
-      # assert response
       assert json_response(conn, 200)
-
-      # checking serialization
+      # assert serialization
       assert json_response(conn, 200) == %{
                "data" => %{
                  "id" => todo.id,
@@ -54,77 +52,97 @@ defmodule KeepWeb.TodoControllerTest do
     end
   end
 
-  describe "todo-create-update-archive" do
-    test "create todo list" do
+  describe "Public API - POST" do
+    test "POST /api/list/create" do
       conn = build_conn()
-      conn = post(conn, Routes.todo_path(conn, :create_list, %{"title" => "Web3"}))
+      conn = post(conn, Routes.todo_path(conn, :create_list, %{title: "Web3"}))
+
       assert json_response(conn, 200)
       json_response = json_response(conn, 200)
       list = Map.get(json_response, "data")
       assert list["title"] == "Web3"
     end
 
-    test "update recently created todo list" do
+    test "POST /api/list/update" do
       conn = build_conn()
-      conn = post(conn, Routes.todo_path(conn, :create_list, %{"title" => "Web3"}))
+      conn = post(conn, Routes.todo_path(conn, :create_list, %{title: "Web3"}))
 
-      assert json_response(conn, 200)
       list = Map.get(json_response(conn, 200), "data")
 
       conn =
         post(
           conn,
           Routes.todo_path(conn, :update_list, %{
-            "title" => "Web2.0 Still works",
-            "list_id" => list["id"]
+            title: "Web2.0 Still works",
+            list_id: list["id"]
           })
         )
 
       assert json_response(conn, 200)
       updated_list = Map.get(json_response(conn, 200), "data")
-      assert list["title"] != updated_list["title"]
+      refute list["title"] == updated_list["title"]
     end
 
-    test "create an item for a todo list" do
+    test "POST /api/list/archived" do
       conn = build_conn()
-      conn = post(conn, Routes.todo_path(conn, :create_list, %{"title" => "Web3"}))
+      conn = post(conn, Routes.todo_path(conn, :create_list, %{title: "Web3"}))
+
+      list = Map.get(json_response(conn, 200), "data")
+
+      conn =
+        post(
+          conn,
+          Routes.todo_path(conn, :update_list_status, %{
+            list_id: list["id"],
+            archived: true
+          })
+        )
+
+      assert json_response(conn, 200)
+      updated_list = Map.get(json_response(conn, 200), "data")
+      assert updated_list["archived"]
+    end
+
+    test "POST /api/list/item/create" do
+      conn = build_conn()
+      conn = post(conn, Routes.todo_path(conn, :create_list, %{title: "Web3"}))
 
       list = Map.get(json_response(conn, 200), "data")
 
       new_item = %{
-        "list_id" => list["id"],
-        "content" => "Decentralization"
+        list_id: list["id"],
+        content: "Decentralization"
       }
 
       conn = post(conn, Routes.todo_path(conn, :create_item, new_item))
+
       assert json_response(conn, 200)
       item = Map.get(json_response(conn, 200), "data")
       assert item["content"] == new_item["content"]
     end
 
-    test "update an item" do
+    test "POST /api/list/item/update" do
       conn = build_conn()
-      conn = post(conn, Routes.todo_path(conn, :create_list, %{"title" => "Web3"}))
+      conn = post(conn, Routes.todo_path(conn, :create_list, %{title: "Web3"}))
 
       list = Map.get(json_response(conn, 200), "data")
 
       new_item = %{
-        "list_id" => list["id"],
-        "content" => "Decentralization"
+        list_id: list["id"],
+        content: "Decentralization"
       }
 
       conn = post(conn, Routes.todo_path(conn, :create_item, new_item))
       item = Map.get(json_response(conn, 200), "data")
 
       update_item = %{
-        "item_id" => item["id"],
-        "content" => "Decentralization - Expensive"
+        item_id: item["id"],
+        content: "Decentralization - Expensive"
       }
 
       conn = post(conn, Routes.todo_path(conn, :update_item, update_item))
-
       updated_item = Map.get(json_response(conn, 200), "data")
-      assert item["content"] != updated_item["content"]
+      refute item["content"] == updated_item["content"]
     end
   end
 end
