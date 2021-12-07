9384165d136debc0b3728e9d55453382d0b5b790
mudssrali <mudassaralics007@gmail.com>
2021-12-07 05:50:37 +0500

    mark item complete and incomplete

--- web/src/components/todo/item-card.tsx
+++ web/src/components/todo/item-card.tsx
@@ -1,9 +1,11 @@
 import { CheckCircleIcon } from '@heroicons/react/outline'
 import clsx from 'clsx'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 interface TodoItemCardProps {
 	item: TodoItem
+	editable: boolean
 	notifyItemUpdate?: (item: TodoItem) => void
 }
 
@@ -12,17 +14,59 @@ type State = {
 	loading: false
 }
 
-const enum ItemActions {
-	MARK_COMPLETED,
-	MARK_INCOMPLETED,
-	EDIT
-}
-
-export const TodoItemCard = ({ item }: TodoItemCardProps) => {
+export const TodoItemCard = ({ item, editable }: TodoItemCardProps) => {
 	const [state, setState] = useState<State>({
 		item,
 		loading: false
 	})
+
+	const toggleTodoItemStatus = () => {
+		if (!editable) {
+			return window.alert('You cannot edit item in archive todo list!')
+		}
+
+		if (state.item.completed && !window.confirm('Are you sure you want to incomplete?')) {
+			return
+		}
+
+		const payload = {
+			item_id: state.item.id,
+			completed: !state.item.completed
+		}
+
+		const requestOptions = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		} as RequestInit
+
+		const toastId = toast.loading(
+			`Marking item ${payload.completed ? 'completed' : 'incomplete'}...`
+		)
+
+		fetch('/api/list/item/completed', requestOptions)
+			.then(res => res.json())
+			.then(
+				(res: ServerResponse) => {
+					console.log(res)
+
+					if (res.code === 200) {
+						toast.dismiss(toastId)
+						setState(prevState => ({ ...prevState, item: res.data as TodoItem }))
+					} else {
+						toast.dismiss(toastId)
+						toast.error(res.error)
+					}
+				},
+				error => {
+					console.log(error)
+					toast.error('Something went wrong.')
+					toast.dismiss(toastId)
+				}
+			)
+	}
 
 	return (
 		<div className="px-2 py-1 rounded-md bg-gray-100">
@@ -30,10 +74,14 @@ export const TodoItemCard = ({ item }: TodoItemCardProps) => {
 				<p>{state.item.content}</p>
 				<div className="">
 					<CheckCircleIcon
-						className={clsx('w-8 h-8', {
-							'text-green-500': item.completed,
-							'text-gray-500': !item.completed
-						})}
+						onClick={toggleTodoItemStatus}
+						className={clsx(
+							'w-8 h-8 cursor-pointer cursor-pointer',
+							state.item.completed ? 'text-green-500' : 'text-gray-500',
+							{
+								'pointer-events-none': !editable
+							}
+						)}
 					/>
 				</div>
 			</div>
